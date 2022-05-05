99b15e91aba8d5144ea3867c441d9aede895c9ba
Oxi <33568643+oxi-dev0@users.noreply.github.com>
2022-05-05 21:13:46 +0100

    Full SFML Implementation and premake :)

--- premake5.lua
+++ premake5.lua
@@ -23,18 +23,41 @@ project "BioSim"
 		"src/**.cpp"
 	}
 
+	includedirs
+	{
+		"vendor/SFML/include/"
+	}
+
+	libdirs { "vendor/SFML/extlibs/libs-msvc-universal/x64", "vendor/SFML/lib" }
+	links { "opengl32.lib", "freetype.lib", "winmm.lib", "gdi32.lib", "openal32.lib", "flac.lib", "vorbisenc.lib", "vorbisfile.lib", "vorbis.lib", "ogg.lib", "ws2_32.lib" }
+
+
 	filter "system:windows"
 		cppdialect "C++20"
 		systemversion "latest"
 
+		defines "SFML_STATIC"
+
 	filter "configurations:Debug"
 		defines "DEBUG"
 		symbols "On"
+		staticruntime "on"
+		runtime "Debug"
+
+		links { "sfml-graphics-s-d.lib", "sfml-system-s-d.lib", "sfml-window-s-d.lib" }
 
 	filter "configurations:Release"
 		defines "RELEASE"
 		optimize "On"
+		staticruntime "on"
+		runtime "Release"
+
+		links { "sfml-graphics-s.lib", "sfml-system-s.lib", "sfml-window-s.lib" }
 
 	filter "configurations:Dist"
 		defines "DIST"
 		optimize "On"
+		staticruntime "on"
+		runtime "Release"
+
+		links { "sfml-graphics-s.lib", "sfml-system-s.lib", "sfml-window-s.lib" }
